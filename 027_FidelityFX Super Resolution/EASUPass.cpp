61bc8c87ac1b344842367d751bde185b52373e96
Veerzeng <1071209504@qq.com>
2021-08-16 15:04:06 +0800

    update FSR

--- 027_FidelityFX Super Resolution/EASUPass.cpp
+++ 027_FidelityFX Super Resolution/EASUPass.cpp
@@ -14,50 +14,30 @@ CEASUPass::~CEASUPass()
 }
 
 
-void CEASUPass::FsrEasuCon(
-	glm::vec4& con0,
-	glm::vec4& con1,
-	glm::vec4& con2,
-	glm::vec4& con3,
-	// This is the resolution of the resource containing the input image (useful for dynamic resolution)
+void CEASUPass::fsrEasuCon(
+	glm::vec4& Con0,
+	glm::vec4& Con1,
+	glm::vec4& Con2,
+	glm::vec4& Con3,
 	float inputSizeInPixelsX,
 	float inputSizeInPixelsY,
-	// This is the display resolution which the input image gets upscaled to
 	float outputSizeInPixelsX,
 	float outputSizeInPixelsY) {
-	// Output integer position to a pixel position in viewport.
-	con0[0] = (inputSizeInPixelsX* 1.0f / (outputSizeInPixelsX));
-	con0[1] = (inputSizeInPixelsY* 1.0f / (outputSizeInPixelsY));
-	con0[2] = ((0.5)*inputSizeInPixelsX* 1.0f / (outputSizeInPixelsX) - (0.5));
-	con0[3] = ((0.5)*inputSizeInPixelsY* 1.0f / (outputSizeInPixelsY) - (0.5));
-	// Viewport pixel position to normalized image space.
-	// This is used to get upper-left of 'F' tap.
-	con1[0] = ( 1.0f / (inputSizeInPixelsX));
-	con1[1] = ( 1.0f / (inputSizeInPixelsY));
-	// Centers of gather4, first offset from upper-left of 'F'.
-	//      +---+---+
-	//      |   |   |
-	//      +--(0)--+
-	//      | b | c |
-	//  +---F---+---+---+
-	//  | e | f | g | h |
-	//  +--(1)--+--(2)--+
-	//  | i | j | k | l |
-	//  +---+---+---+---+
-	//      | n | o |
-	//      +--(3)--+
-	//      |   |   |
-	//      +---+---+
-	con1[2] = ((1.0)* 1.0f / (inputSizeInPixelsX));
-	con1[3] = ((-1.0)* 1.0f / (inputSizeInPixelsY));
-	// These are from (0) instead of 'F'.
-	con2[0] = ((-1.0)* 1.0f / (inputSizeInPixelsX));
-	con2[1] = ((2.0)* 1.0f / (inputSizeInPixelsY));
-	con2[2] = ((1.0)* 1.0f / (inputSizeInPixelsX));
-	con2[3] = ((2.0)* 1.0f / (inputSizeInPixelsY));
-	con3[0] = ((0.0)* 1.0f / (inputSizeInPixelsX));
-	con3[1] = ((4.0)* 1.0f / (inputSizeInPixelsY));
-	con3[2] = con3[3] = 0;
+	Con0[0] = (inputSizeInPixelsX* 1.0f / (outputSizeInPixelsX));
+	Con0[1] = (inputSizeInPixelsY* 1.0f / (outputSizeInPixelsY));
+	Con0[2] = ((0.5)*inputSizeInPixelsX* 1.0f / (outputSizeInPixelsX) - (0.5));
+	Con0[3] = ((0.5)*inputSizeInPixelsY* 1.0f / (outputSizeInPixelsY) - (0.5));
+	Con1[0] = ( 1.0f / (inputSizeInPixelsX));
+	Con1[1] = ( 1.0f / (inputSizeInPixelsY));
+	Con1[2] = ((1.0)* 1.0f / (inputSizeInPixelsX));
+	Con1[3] = ((-1.0)* 1.0f / (inputSizeInPixelsY));
+	Con2[0] = ((-1.0)* 1.0f / (inputSizeInPixelsX));
+	Con2[1] = ((2.0)* 1.0f / (inputSizeInPixelsY));
+	Con2[2] = ((1.0)* 1.0f / (inputSizeInPixelsX));
+	Con2[3] = ((2.0)* 1.0f / (inputSizeInPixelsY));
+	Con3[0] = ((0.0)* 1.0f / (inputSizeInPixelsX));
+	Con3[1] = ((4.0)* 1.0f / (inputSizeInPixelsY));
+	Con3[2] = Con3[3] = 0;
 }
 
 void CEASUPass::initV()
@@ -80,18 +60,18 @@ void CEASUPass::initV()
 	TextureConfig4EASU->Width = m_DisplayWidth;
 	TextureConfig4EASU->Height = m_DisplayHeight;
 	genTexture(TextureConfig4EASU);
-	glm::vec4 con0 = glm::vec4(0);
-	glm::vec4 con1 = glm::vec4(0);
-	glm::vec4 con2 = glm::vec4(0);
-	glm::vec4 con3 = glm::vec4(0);
-	FsrEasuCon(con0, con1, con2, con3, m_RenderWidth, m_RenderHeight,m_DisplayWidth, m_DisplayHeight);
+	glm::vec4 Con0 = glm::vec4(0);
+	glm::vec4 Con1 = glm::vec4(0);
+	glm::vec4 Con2 = glm::vec4(0);
+	glm::vec4 Con3 = glm::vec4(0);
+	fsrEasuCon(Con0, Con1, Con2, Con3, m_RenderWidth, m_RenderHeight,m_DisplayWidth, m_DisplayHeight);
 	m_pShader = std::make_shared<CShader>("EASUPass_CS.glsl");
 	m_pShader->activeShader();
 	m_pShader->setTextureUniformValue("u_InputTexture", m_InputTexture);
-	m_pShader->setFloatUniformValue("con0", con0.x, con0.y, con0.z, con0.w);
-	m_pShader->setFloatUniformValue("con1", con1.x, con1.y, con1.z, con1.w);
-	m_pShader->setFloatUniformValue("con2", con2.x, con2.y, con2.z, con2.w);
-	m_pShader->setFloatUniformValue("con3", con3.x, con3.y, con3.z, con3.w);
+	m_pShader->setFloatUniformValue("u_Con0", Con0.x, Con0.y, Con0.z, Con0.w);
+	m_pShader->setFloatUniformValue("u_Con1", Con1.x, Con1.y, Con1.z, Con1.w);
+	m_pShader->setFloatUniformValue("u_Con2", Con2.x, Con2.y, Con2.z, Con2.w);
+	m_pShader->setFloatUniformValue("u_Con3", Con3.x, Con3.y, Con3.z, Con3.w);
 	std::vector<int> LocalGroupSize;
 	m_pShader->InquireLocalGroupSize(LocalGroupSize);
 	m_GlobalGroupSize.push_back((m_DisplayWidth + LocalGroupSize[0] - 1) / LocalGroupSize[0]);
@@ -99,7 +79,7 @@ void CEASUPass::initV()
 	m_GlobalGroupSize.push_back(1);
 	ElayGraphics::ResourceManager::registerSharedData("EASUTexture", TextureConfig4EASU);
 	ElayGraphics::ResourceManager::registerSharedData("NormalTexture", m_InputTexture);
-	ElayGraphics::ResourceManager::registerSharedData("con", con0);
+	ElayGraphics::ResourceManager::registerSharedData("Const", Con0);
 }
 
 void CEASUPass::updateV()
