da26ba913688bb69a0e94fa197a9648f9953c6c3
Veerzeng <1071209504@qq.com>
2021-08-23 15:30:20 +0800

    Finish FSR

--- 027_FidelityFX Super Resolution/main.cpp
+++ 027_FidelityFX Super Resolution/main.cpp
@@ -7,7 +7,7 @@ int main()
 	ElayGraphics::WINDOW_KEYWORD::setWindowSize(1920, 1080);
 	ElayGraphics::WINDOW_KEYWORD::setIsCursorDisable(false);
 	ElayGraphics::COMPONENT_CONFIG::setIsEnableGUI(false);
-
+	ElayGraphics::WINDOW_KEYWORD::setWindowTile("O:switch to Normal Texture;  P:switch to FSR Texture");
 	ElayGraphics::ResourceManager::registerRenderPass(std::make_shared<CEASUPass>("EASUPass", 0));
 	ElayGraphics::ResourceManager::registerRenderPass(std::make_shared<CRCASPass>("RCASPass", 1));
 	ElayGraphics::ResourceManager::registerRenderPass(std::make_shared<CScreenQuadPass>("ScreenQuadPass", 2));
